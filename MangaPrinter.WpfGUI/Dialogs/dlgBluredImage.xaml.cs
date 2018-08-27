ca2f54a700f09d953c21dfa80379bd1de787d0c3
Yoni Was (yoga-lap) <yonixw@gmail.com>
2018-08-27 21:53:42 +0300

    moving picture with drag-like gesture

--- MangaPrinter.WpfGUI/Dialogs/dlgBluredImage.xaml.cs
+++ MangaPrinter.WpfGUI/Dialogs/dlgBluredImage.xaml.cs
@@ -23,5 +23,49 @@ namespace MangaPrinter.WpfGUI.Dialogs
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bm = new BitmapImage(new Uri
+                (@"C:\Users\Yoni\source\repos\MangaPrinter\MangaPrinter.WpfGUI\Icons\1Page.png", UriKind.Absolute));
+            imgMain.Width = bm.Width;
+            imgMain.Height = bm.Height;
+            imgMain.Source = bm;
+        }
+
+        bool inMove = false;
+        Point startPointMouse, startPointWindow;
+        private void imgMain_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            inMove = true;
+            startPointMouse = Mouse.GetPosition(this);
+            startPointWindow = new Point(Canvas.GetLeft(imgMain), Canvas.GetTop(imgMain));
+        }
+
+        private void cnvsImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (inMove)
+            {
+                Point currentMouse = Mouse.GetPosition(this);
+                Canvas.SetLeft(imgMain, currentMouse.X - startPointMouse.X + startPointWindow.X);
+                Canvas.SetTop(imgMain, currentMouse.Y - startPointMouse.Y + startPointWindow.Y);
+            }
+        }
+
+        private void cnvsImage_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Point currentMouse = Mouse.GetPosition(cnvsImage);
+            // only stop if leaving to outside (not back to image)
+            if (currentMouse.X > 0 && currentMouse.X < cnvsImage.ActualWidth && currentMouse.Y > 0 && currentMouse.Y < cnvsImage.ActualHeight)
+            { }  
+            else
+                inMove = false;
+
+        }
+
+        private void imgMain_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            inMove = false;
+        }
     }
 }
