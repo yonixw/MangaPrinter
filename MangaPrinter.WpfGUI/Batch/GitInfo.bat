9c4463583c4fa92cfae28d36482be2992e4a5ec8
Yoni Was (yoga-lap) <yonixw@gmail.com>
2018-09-03 01:29:09 +0300

    commit count works + show * if changes since last commit

--- MangaPrinter.WpfGUI/Batch/GitInfo.bat
+++ MangaPrinter.WpfGUI/Batch/GitInfo.bat
@@ -6,7 +6,7 @@ FOR /F "tokens=*" %%g IN ('git rev-parse --abbrev-ref HEAD') do (
 	SET branch=%%g
 )
 
-FOR /F "tokens=* usebackq" %%g IN (`git show --no-patch --format"=%%h, %%ci, %%f"`) do (
+FOR /F "tokens=* usebackq" %%g IN (`git show --no-patch --format"=%%h, %%ci; %%f"`) do (
 	SET commitinfo=%%g
 )
 
@@ -14,8 +14,13 @@ FOR /F "tokens=* usebackq" %%g IN (`git describe --abbrev"=0" --tags`) do (
 	SET latesttag=%%g
 )
 
-FOR /F "tokens=*" %%g IN ('git rev-list --count %latesttag%') do (
+FOR /F "tokens=*" %%g IN ('git rev-list --count %latesttag%..') do (
 	SET commitsafter=%%g
 )
 
-echo %latesttag%+%commitsafter% (%branch%) %commitinfo% > "%projectdir%Resources\GitInfo.txt"	
+FOR /F "tokens=*" %%g IN ('git status --porcelain') do (
+	SET dirty=*
+)
+
+
+echo %latesttag%+%commitsafter%%dirty% (%branch%) %commitinfo% > "%projectdir%Resources\GitInfo.txt"	
