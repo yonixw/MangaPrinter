bb78f4e773c1b925e1589813d27e51fe00c4167b
Yoni Was (yoga-lap) <yonixw@gmail.com>
2018-09-07 01:24:39 +0300

    BeforeDouble_RTL, MAKE_EVEN_DOUBLE_RTL ,MAKE_EVEN_SINGLE_RTL all pass. look like algo is working!

--- MangaPrinter.Core_Tests/DuplexChapterBuilder_Tests.cs
+++ MangaPrinter.Core_Tests/DuplexChapterBuilder_Tests.cs
@@ -13,7 +13,7 @@ namespace MangaPrinter.Core.Tests
     [TestClass()]
     public class DuplexChapterBuilder_Tests
     {
-        
+
         [TestMethod()]
         public void DuplexTest1_SimpleDouble_EvenPages_RTL()
         {
@@ -30,12 +30,12 @@ namespace MangaPrinter.Core.Tests
                     Front = new VerifyFace()
                     {
                         Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 0},
-                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=1},   
+                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=1},
                     },
                      Back = new VerifyFace()
                     {
                          Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 2},
-                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=2},   
+                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=2},
                     }
                 }
             });
@@ -73,5 +73,138 @@ namespace MangaPrinter.Core.Tests
             if (Utils.isErorr(error))
                 Assert.Fail(error);
         }
-    } 
+
+        [TestMethod()]
+        public void DuplexTest1_SimpleDouble_BeforeDouble_RTL()
+        {
+            MangaChapter ch = Utils.MockChapter(true, new bool[]
+            {
+                false, true, false, true
+            });
+
+            PrintChapter pc = new ChapterBuilders.DuplexChapterBuilder().Build(ch);
+
+            string error = Utils.VerifyChapter(ch, pc, new VerifyPage[] {
+                new VerifyPage()
+                {
+                    Front = new VerifyFace()
+                    {
+                        Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 0},
+                         Left = new VerifySide() { type = SingleSideType.BEFORE_DOUBLE }
+                    },
+                     Back = new VerifyFace()
+                    {
+                         Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=1},
+                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=1},
+                    }
+                },
+                new VerifyPage()
+                {
+                    Front = new VerifyFace()
+                    {
+                        Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 2},
+                         Left = new VerifySide() { type = SingleSideType.BEFORE_DOUBLE},
+                    },
+                     Back = new VerifyFace()
+                    {
+                         Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=3},
+                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=3},
+                    }
+                }
+            });
+
+            if (Utils.isErorr(error))
+                Assert.Fail(error);
+        }
+
+        [TestMethod()]
+        public void DuplexTest1_SimpleDouble_MAKE_EVEN_DOUBLE_RTL()
+        {
+            MangaChapter ch = Utils.MockChapter(true, new bool[]
+            {
+                false, false, false, true
+            });
+
+            PrintChapter pc = new ChapterBuilders.DuplexChapterBuilder().Build(ch);
+
+            string error = Utils.VerifyChapter(ch, pc, new VerifyPage[] {
+                new VerifyPage()
+                {
+                    Front = new VerifyFace()
+                    {
+                        Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 0},
+                         Left = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 1}
+                    },
+                     Back = new VerifyFace()
+                    {
+                         Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=2},
+                         Left = new VerifySide() { type = SingleSideType.BEFORE_DOUBLE},
+                    }
+                },
+                new VerifyPage()
+                {
+                    Front = new VerifyFace()
+                    {
+                          Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=3},
+                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=3},
+                    },
+                     Back = new VerifyFace()
+                    {
+                         Right = new VerifySide() {type = SingleSideType.MAKE_EVEN},
+                         Left = new VerifySide() { type = SingleSideType.MAKE_EVEN},
+                    }
+                }
+            });
+
+            if (Utils.isErorr(error))
+                Assert.Fail(error);
+        }
+
+        [TestMethod()]
+        public void DuplexTest1_SimpleDouble_MAKE_EVEN_SINGLE_RTL()
+        {
+            MangaChapter ch = Utils.MockChapter(true, new bool[]
+            {
+                false, false, false, true,false
+            });
+
+            PrintChapter pc = new ChapterBuilders.DuplexChapterBuilder().Build(ch);
+
+            string error = Utils.VerifyChapter(ch, pc, new VerifyPage[] {
+                new VerifyPage()
+                {
+                    Front = new VerifyFace()
+                    {
+                        Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 0},
+                         Left = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex = 1}
+                    },
+                     Back = new VerifyFace()
+                    {
+                         Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=2},
+                         Left = new VerifySide() { type = SingleSideType.BEFORE_DOUBLE},
+                    }
+                },
+                new VerifyPage()
+                {
+                    Front = new VerifyFace()
+                    {
+                          Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=3},
+                         Left = new VerifySide() { type = SingleSideType.MANGA, MangaPageSourceIndex=3},
+                    },
+                     Back = new VerifyFace()
+                    {
+                         Right = new VerifySide() {type = SingleSideType.MANGA, MangaPageSourceIndex=4},
+                         Left = new VerifySide() { type = SingleSideType.MAKE_EVEN},
+                    }
+                }
+            });
+
+            if (Utils.isErorr(error))
+                Assert.Fail(error);
+        }
+
+    }
+
+
+
 }
