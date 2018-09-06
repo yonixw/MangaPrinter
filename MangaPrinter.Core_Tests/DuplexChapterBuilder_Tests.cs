1ca20828a7e2847f5d8a9f4c7c64d1075c701db3
Yoni Was (yoga-lap) <yonixw@gmail.com>
2018-09-06 22:19:41 +0300

    add unit test... testing duplex

---
+++ MangaPrinter.Core_Tests/DuplexChapterBuilder_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MangaPrinter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaPrinter.Core.Tests
+{
+    [TestClass()]
+    public class DuplexChapterBuilder_Tests
+    {
+        [TestMethod()]
+        public void Build_Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
