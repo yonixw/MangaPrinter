0afdbf52d766e0aa6cf3a1a6fda14c33de7e6f3f
Yoni Was (yoga-lap) <yonixw@gmail.com>
2018-12-19 01:14:48 +0200

    template builder complete! now export images + convert to pdf

--- MangaPrinter.Core/TemplateBuilders/DuplexTemplates.cs
+++ MangaPrinter.Core/TemplateBuilders/DuplexTemplates.cs
@@ -58,7 +58,7 @@ namespace MangaPrinter.Core.TemplateBuilders
                         string.Format("Got type {0} in left single in duplex. It's unexpected.", face.Left.SideType)
                         );
 
-                return null;
+                return TemplateSingle(face, nextFace, spW, spH, padding);
             }
         }
 
@@ -83,8 +83,8 @@ namespace MangaPrinter.Core.TemplateBuilders
             using (Graphics g = Graphics.FromImage(b))
             {
                 int sideIndex = face.Right.SideNumber;
-                string side1 = "[" + sideIndex + "]";
-                string side2 = "[" + (sideIndex + 1) + "]";
+                string side1 = "[ " + sideIndex + " ]";
+                string side2 = "[ " + (sideIndex + 1) + " ]";
                 Font side1Font = GraphicsUtils.FindFontSizeByContent(
                     g, side1, new Size(sideTextW, sideTextH), fontSide);
                 Font side2Font = GraphicsUtils.FindFontSizeByContent(
@@ -127,13 +127,97 @@ namespace MangaPrinter.Core.TemplateBuilders
                 page.Dispose();
 
                 // border
-                g.DrawRectangle(blackPen, new Rectangle(padding, padding, contentW, contentH));
+                //g.DrawRectangle(blackPen, new Rectangle(padding, padding, contentW, contentH));
             }
 
             return b;
         }
 
+        private Bitmap TemplateSingle(PrintFace face, PrintFace nextFace, int spW, int spH, int padding)
+        {
+            int tmpW = spW * 2;
+            int tmpH = spH;
+
+            int arrowW = tmpW - padding * 2;
+            int arrowH = padding;
+
+            int pageW = (tmpW - padding * 3) /2; // one padding between pages
+            int pageH = tmpH - padding * 2;
+
+            int sideTextW = pageW;
+            int sideTextH = padding;
+
+            bool isRTL = face.IsRTL; 
+
+            Bitmap b = new Bitmap(tmpW, tmpH);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                string sideLeft = "[ " + face.Left.SideNumber + " ]";
+                string sideRight = "[ " + face.Right.SideNumber + " ]";
+                Font sideLeftFont = GraphicsUtils.FindFontSizeByContent(
+                    g, sideLeft, new Size(sideTextW, sideTextH), fontSide);
+                Font sideRightFont = GraphicsUtils.FindFontSizeByContent(
+                    g, sideRight, new Size(sideTextW, sideTextH), fontSide);
+
+                if (isRTL)
+                {
+                    GraphicsUtils.DrawArrowHeadRow(g, blackPen,
+                        new Point(tmpW - padding, padding / 2),
+                        new Point(padding, padding / 2),
+                        padding);
+                }
+                else
+                {
+                    GraphicsUtils.DrawArrowHeadRow(g, blackPen,
+                       new Point(padding, padding / 2),
+                       new Point(tmpW - padding, padding / 2),
+                       padding);
+                }
+
+                g.DrawString(sideLeft, sideLeftFont, blackBrush, new PointF(padding, padding + pageH));
+                g.DrawString(sideRight, sideRightFont, blackBrush, new PointF(tmpW / 2, padding + pageH));
 
 
+                DrawSide(pageW, pageH, g, face.Left, new Point(padding, padding));
+                DrawSide(pageW, pageH, g, face.Right, new Point(padding*2 + pageW, padding));
+
+                // border
+                //g.DrawRectangle(blackPen, new Rectangle(padding, padding, pageW, pageH));
+            }
+
+            return b;
+        }
+
+        private void DrawSide(int pageW, int pageH, Graphics g,  PrintSide side, Point pagePlace)
+        {
+            Bitmap page = null;
+            switch (side.SideType)
+            {
+                case SingleSideType.INTRO:
+                    page = GraphicsUtils.createImageWithText(sideTextConsts[SingleSideType.INTRO]
+                        .Replace("{0}", side.MangaPageSource.Chapter.Name),
+                      pageH, pageW);
+                    break;
+                case SingleSideType.OUTRO:
+                    page = GraphicsUtils.createImageWithText(sideTextConsts[SingleSideType.OUTRO]
+                        .Replace("{0}", side.MangaPageSource.Chapter.Name),
+                      pageH, pageW);
+                    break;
+                case SingleSideType.BEFORE_DOUBLE:
+                    page = GraphicsUtils.createImageWithText(sideTextConsts[SingleSideType.BEFORE_DOUBLE],
+                      pageH, pageW);
+                    break;
+                case SingleSideType.MAKE_EVEN:
+                    page = GraphicsUtils.createImageWithText(sideTextConsts[SingleSideType.MAKE_EVEN],
+                       pageH, pageW);
+                    break;
+                case SingleSideType.MANGA:
+                    page = GraphicsUtils.loadFileZoomedCentered(side.MangaPageSource.ImagePath,
+                       pageH, pageW);
+                    break;
+            }
+            g.DrawImage(page, pagePlace);
+            page.Dispose();
+        }
     }
 }
