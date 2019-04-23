e5f96d0359041f323a895040ec4d54500e5fc300
Yoni Was (yoga-lap) <yonixw@gmail.com>
2019-04-23 23:37:52 +0300

    On second thought, 1.15 will make more decorative stuff as single pages (like recruiting translator to fan sub etc)

--- MangaPrinter.WpfGUI/Properties/Settings.Designer.cs
+++ MangaPrinter.WpfGUI/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace MangaPrinter.WpfGUI.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.85")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.15")]
         public float doublePageWidth {
             get {
                 return ((float)(this["doublePageWidth"]));
