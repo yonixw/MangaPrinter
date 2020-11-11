671b85a24b1fb200572190c14b3cfb58ae85c10b
yoni was <yonixw@gmail.com>
2020-11-11 21:34:58 +0200

    client to react

--- client/src/lib/MangaObjects.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class MangaPage {
-    isDouble: boolean;
-}
-export declare class MangaChapter {
-    name: string;
-    rtl: boolean;
-    pages: Array<MangaPage>;
-    static mockChapter(name: string, rtl: boolean, pageCount: number): MangaChapter;
-}
--- react/src/ctrls/items/chapteritem/ctrl.stories.tsx
+++ react/src/ctrls/items/chapteritem/ctrl.stories.tsx
@@ -20,5 +20,29 @@ RTL.args = {
   chapter: MangaChapter.mockChapter(
     "Chapter1",
     true,
+    20)
+};
+
+export const LTR = Template.bind({});
+LTR.args = {
+  chapter: MangaChapter.mockChapter(
+    "Chapter2",
+    true,
+    24)
+};
+
+export const PageCountWarn1 = Template.bind({});
+PageCountWarn1.args = {
+  chapter: MangaChapter.mockChapter(
+    "Chapter3",
+    false,
     25)
+};
+
+export const PageCountWarn2 = Template.bind({});
+PageCountWarn2.args = {
+  chapter: MangaChapter.mockChapter(
+    "Chapter4",
+    true,
+    65)
 };
