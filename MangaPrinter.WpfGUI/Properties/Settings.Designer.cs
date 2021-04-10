ca430d775414dc750a83f04046cad18d6588a380
yoni was <yonixw@gmail.com>
2021-04-11 01:26:40 +0300

    name by default but correct from setting file

--- MangaPrinter.WpfGUI/Properties/Settings.Designer.cs
+++ MangaPrinter.WpfGUI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MangaPrinter.WpfGUI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -34,7 +34,7 @@ namespace MangaPrinter.WpfGUI.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool orderImportByName {
             get {
                 return ((bool)(this["orderImportByName"]));
