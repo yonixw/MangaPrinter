600986ef4700d79741a2881814d7784f43c31636
yoni was <yonixw@gmail.com>
2023-07-30 20:46:10 +0300

    התחלנו

---
+++ MangaPrinter.Core/ChapterBuilders/BookletBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaPrinter.Core.ChapterBuilders
+{
+    class BookletBinder : IBindBuilder
+    {
+        public List<PrintPage> Build(List<MangaChapter> ch, bool startPage, bool endPage, int antiSpoiler = 0)
+        {
+            
+        }
+    }
+}
